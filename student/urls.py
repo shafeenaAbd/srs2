f2e80393f5b90bddfce152748b4eef485bdf7e7a
shafeenaAbd <shafeena@mod.gov.my>
2018-05-24 15:41:57 +0800

    Put comment in setting file

--- student/urls.py
+++ student/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$',views.home,name='student_home'),
     url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"),
+    url(r'^home/$',views.home_json,name='student_home_json'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib import messages
+# from django.utils import timezone
 from .models import Student
-
+# from .forms import MessageForm, SearchForm, StudentForm
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django_datatables_view.base_datatable_view import BaseDatatableView
+from django.db.models import Count, Sum, Q, Case, Value, When, IntegerField
 # Create your views here.
 
 def home(request):
 	return render(request,'base.html')
+
+def home_json(request):
+    return render(request, 'student/home_json.html')
 # Create your views here.
 # Student JSON list filtering
 class student_list_json(BaseDatatableView):
